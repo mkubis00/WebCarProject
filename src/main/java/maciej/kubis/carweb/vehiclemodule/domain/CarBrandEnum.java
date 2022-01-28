86c4846556194df191f87ff773adebba7fb1503b
Maciej Kubis <maciej.k-2000@outlook.com>
2022-01-28 22:40:42 +0100

    CarBrandEnum and CarTypeEnum update

--- src/main/java/maciej/kubis/carweb/vehiclemodule/domain/CarBrandEnum.java
+++ src/main/java/maciej/kubis/carweb/vehiclemodule/domain/CarBrandEnum.java
@@ -2,7 +2,11 @@ package maciej.kubis.carweb.vehiclemodule.domain;
 
 public enum CarBrandEnum {
 
-    ASTON_MARTIN("Aston Martin"), AUDI("AUDI"), BAC("Bac");
+    ASTON_MARTIN("Aston Martin"), AUDI("AUDI"), BAC("Bac"), BWM("Bwm"), BUICK("Buick"), CHEVROLET("Chevrolet"),
+    FERRARI("Ferrari"), FORD("Ford"), HOONIGAN("Hoonigan"), JAGUAR("Jaguar"), KOENIGSEGG("Koenigsegg"),
+    LAMBORGHINI("Lamborghini"), LOTUS("Lotus"), MAZDA("Mazda"), MCLAREN("Mclaren"), MERCEDES_BEZN("Mercedes Benz"),
+    MINI("Mini"), MORGAN("Morgan"), NISSAN("Nissan"), PAGANI("Pagani"), PORSHE("Porshe"), RENAULT("Renault"),
+    TOYOTA("Toyota"), VOLKSWAGEN("Volkswagen");
 
     private final String value;
 
--- src/main/java/maciej/kubis/carweb/vehiclemodule/domain/CarTypeEnum.java
+++ src/main/java/maciej/kubis/carweb/vehiclemodule/domain/CarTypeEnum.java
@@ -2,7 +2,11 @@ package maciej.kubis.carweb.vehiclemodule.domain;
 
 public enum CarTypeEnum {
 
-    ASTON_MARTIN("Aston Martin"), AUDI("AUDI"), BAC("Bac"); // DO ZMIANY
+    BUGGIES("Buggies"), CLASS_SPORT_CARS("Classic Sports Cars"), CLASSIC_MUSCLE("Classic Muscle"), DRIFT_CARS("Drift Cars"),
+    RALLY_MONSTERS("Rally Monsters"), RETRO_SUPER_CARS("Retro Super Cars"), SUPER_GT ("Super GT"), TRACK_TOYS("Track Toys"),
+    TRUCKS("Trucks"), HYPERCARS("Hypercars"), HOT_HATCH("Hot Hatch"), MORDER_MUSCLE("Modern Muscle"), OFFROAD("Offroad"),
+    VINTAGE_RACERS("Vintage Racers"), VANS_UTILITY("Vans & Utility"),  UTS("UTV's"),  UNLIMITED_OFFROAD("Unlimited Offroad"),
+    UNLIMITED_BUGGIES("Unlimited Buggies");
 
     private final String value;
 
